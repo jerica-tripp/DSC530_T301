85305807ced968b7b93d3ffa723b2897a5a3763f
jerica-tripp <53457553+jerica-tripp@users.noreply.github.com>
2021-06-20 20:43:38 -0500

    Exercise 2.1
    Week 2 Exercise 2.1

---
+++ JTripp_DSC530_Ex21.py
@@ -0,0 +1,36 @@
+#  Jerica Tripp
+#  DSC DSC530-T301 Data Exploration and Analysis (2217-1)
+#  Week 2 Exercises
+#  2021/06/20
+
+
+# Display the text “Hello World! I wonder why that is always the default coding text to start with”
+print("Hello World! I wonder why that is always the default coding text to start with")
+
+# Add two numbers together
+print(2 + 2)
+
+# Subtract a number from another number
+print(2 - 2)
+
+# Multiply two numbers
+print(2 * 2)
+
+# Divide between two numbers
+print(2 / 2)
+
+# Concatenate two strings together (any words)
+print("two"+"two")
+
+# Create a list of 4 items (can be strings, numbers, both)
+lst=[2, 2, "two", "two"]
+print(lst)
+
+# Append an item to your list (again, can be a string, number)
+lst.append("three")
+print(lst)
+
+# Create a tuple with 4 items (can be strings, numbers, both)
+tpl = ("one", "two", "three", "four")
+print(tpl)
+
